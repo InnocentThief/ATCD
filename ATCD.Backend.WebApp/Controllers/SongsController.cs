7b19c3745472964465570ba4f4260ae404c5567e
InnocentThief <github@krebser.com>
2023-02-23 16:59:54 +0100

    Added dto project and first entities

---
+++ ATCD.Backend.WebApp/Controllers/SongsController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ATCD.Backend.WebApp.Controllers
+{
+    [Route("api/songs")]
+    [ApiController]
+    public class SongsController : ControllerBase
+    {
+    }
+}
---
+++ ATCD.DataAccess.Entity/Song.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATCD.DataAccess.Entity
+{
+    internal class Song
+    {
+    }
+}
